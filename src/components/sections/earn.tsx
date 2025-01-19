a726d07bae3e2fcbc5feb1a766ff4b9e8c1402cf
stefan.balea <stefan.balea@axsys.ro>
2025-01-19 02:43:56 +0200

    staking link to explorer instead of docs

--- src/components/sections/earn.tsx
+++ src/components/sections/earn.tsx
@@ -36,7 +36,7 @@ export const Earn = () => {
         <EarnItem 
           heading="Stake $BZE"
           body="Delegate your $BZE to one or multiple validators participating in the BeeZee network. Be sure to claim and stake your rewards periodically to maximize your earnings."
-          url="https://docs.getbze.com"
+          url="https://explorer.getbze.com/beezee/staking"
         />
         <EarnItem 
           heading="Provide Liquidity on Osmosis"
